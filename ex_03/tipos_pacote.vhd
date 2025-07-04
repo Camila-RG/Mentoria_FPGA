685227474621b39da059b631ee28918ee2ff1a2c
Camila Gomes <camilaramosgomes@gmail.com>
2025-07-03 23:15:34 -0300

    Delete ex_03/tipos_pacote.vhd

--- ex_03/tipos_pacote.vhd
+++
@@ -1,8 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Pacote para declarar tipos customizados que serão usados no projeto
-package tipos_pacote is
-    -- Define um tipo "t_integer_array" como um vetor de inteiros
-    type t_integer_array is array (natural range <>) of integer;
-end package tipos_pacote;
